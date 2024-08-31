f69da1d62ef98dcbf17e9c8f0dc19723eea1083a
khushi singhal <khushi.mitr03@gmail.com>
2024-08-31 11:36:00 +0530

    Added functionality to create cpp files, and also the metadata.

--- lua/lazycf/init.lua
+++ lua/lazycf/init.lua
@@ -1,4 +1,7 @@
-local function on_new_connection(stream)
+local Problem = require("lazycf.problem")
+local config = require("lazycf.config"):new()
+
+local function on_new_connection(stream, cwd)
 	local buffer = ""
 
 	stream:read_start(function(err, data)
@@ -23,8 +26,57 @@ local function on_new_connection(stream)
 					-- 	stream:write("Invalid JSON\n")
 					-- else
 					-- Print JSON data to Neovim
+
 					print("Received JSON : " .. vim.json.encode(json_data) .. "\n")
 					stream:write("JSON data received\n")
+
+					local problem = Problem:new(
+						json_data.name,
+						json_data.url,
+						json_data.interactive,
+						json_data.memoryLimit,
+						json_data.timeLimit,
+						json_data.group,
+						json_data.tests,
+						"srcPath",
+						"local"
+					)
+
+					-- giveTestIds() is a function that assigns test ids to the test cases
+					problem:giveTestIds()
+
+					-- extractNameFromUrl() is a function that extracts the problem name from the url
+					problem:extractNameFromUrl()
+
+					local defaultLanguage = config.defaultLanguage
+
+					local sourcePath = cwd .. "/" .. problem.fname .. defaultLanguage
+					problem.srcPath = sourcePath
+
+					problem:print()
+
+					if io.open(sourcePath, "r") then
+						print("File already exists")
+					else
+						print("File does not exist")
+						local file = io.open(sourcePath, "w")
+						file:close()
+					end
+
+					-- open the file created above
+					vim.uv.fs_open(sourcePath, "r", 438, function(err, fd)
+						if err then
+							print("Error opening file: " .. err)
+							return
+						end
+						vim.schedule(function()
+							vim.cmd("edit " .. sourcePath)
+						end)
+					end)
+
+					-- create .cph folder and write metadata file in it
+					problem:writeProblemToJson(cwd)
+
 					-- end
 				else
 					stream:write("Only POST requests are supported\n")
@@ -32,7 +84,9 @@ local function on_new_connection(stream)
 
 				-- Close connection after processing
 				stream:shutdown()
+
 				stream:close()
+				print("stream closed, now you start coding!!")
 			end
 		end
 	end)
@@ -45,6 +99,8 @@ local uv = vim.uv
 -- Function to start the TCP server
 local function start_server()
 	local server = uv.new_tcp()
+	local cwd = vim.fn.getcwd()
+	-- config:setCwd(cwd)
 
 	-- Bind to the port
 	server:bind("127.0.0.1", 27121)
@@ -60,7 +116,7 @@ local function start_server()
 		-- Accept new connections
 		local client = uv.new_tcp()
 		server:accept(client)
-		on_new_connection(client)
+		on_new_connection(client, cwd)
 	end)
 end
 
